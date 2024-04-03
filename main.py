046fcb0cf1b2ea61a2d84c945b0318c88f399f7e
Praveen Vasanthan <86035346+vasanth165@users.noreply.github.com>
2024-04-03 17:17:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/songs",methods=["POST"])
+def show_songs():
+    date = request.form['date']
+    url = f"https://www.billboard.com/charts/hot-100/{date}/"
+
+    response = requests.get(url=url)
+    content = response.text
+
+    soup = BeautifulSoup(content, "html.parser") 
+    all_title = soup.find_all(name="ul", class_="lrv-a-unstyle-list lrv-u-flex lrv-u-height-100p lrv-u-flex-direction-column@mobile-max")
+
+    title = [i.find(name="h3").get_text().strip() for i in all_title]
+
+    if len(title) == 100:
+        return render_template("songs.html", title=title, date=date)
+    else:
+        return render_template("error_page.html", date=date)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
